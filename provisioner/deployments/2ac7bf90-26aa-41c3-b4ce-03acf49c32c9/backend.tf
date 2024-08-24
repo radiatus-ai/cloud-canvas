0053e451dd126510b8f727c7785d15a792ae6834
Will <651833+WillBeebe@users.noreply.github.com>
2024-08-24 12:40:05 -0700

    move provisioner to own repo: https://github.com/radiatus-ai/package-provisioner

--- provisioner/deployments/2ac7bf90-26aa-41c3-b4ce-03acf49c32c9/backend.tf
+++
@@ -1,7 +0,0 @@
-
-terraform {
-  backend "gcs" {
-    bucket = "rad-provisioner-state-1234"
-    prefix = "projects/f65e6aec-7716-41ae-a659-d8a2a0de2d32/packages/2ac7bf90-26aa-41c3-b4ce-03acf49c32c9"
-  }
-}
